ed7dbe7f4b98d9a77b94ccb80407b7eea397f22f
deepakrajm <deepakraj.m@shield.com>
2024-07-09 22:35:38 +0530

    new version is updated

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ShieldLocationAnalysisSPM",
-            url: "https://s3.amazonaws.com/cashshield-sdk/shieldlocation-spm-ios-swift-1-0-0.zip",
-            checksum: "d2013927ad77af34f06c8f124c918107814e9c22b7a34bfa501e774dbf6450b4"
+            url: "https://s3.amazonaws.com/cashshield-sdk/shieldlocation-spm-ios-swift-1-0-1.zip",
+            checksum: "a6e9072f8daeb93b731fe614f68dd922cf3cfa38620724dfda0f322d5d134119"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
