40a4e00cae1a00872f12507b9c2de79bcc3ccb46
deepakrajm <deepakraj.m@shield.com>
2024-07-22 23:53:04 +0530

    version updated to 1.0.3

--- Package.swift
+++ Package.swift
@@ -22,15 +22,10 @@ let package = Package(
             url: "https://s3.amazonaws.com/cashshield-sdk/shieldlocation-spm-ios-swift-1-0-2.zip",
             checksum: "8e7d9c8dc59b5a8b0e721d2eb60d7fb66717d007c781eb99779a7fbdb034b9aa"
         ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            path: "Frameworks/SwiftProtobuf.xcframework"
-        ),
         .target(
             name: "ShieldLocationLibrary",
             dependencies: [
                 "ShieldLocationAnalysisSPM",
-                "SwiftProtobuf",
                 .product(name: "ShieldFraud", package: "shieldfraud-spm")
             ],
             path: "Sources/ShieldLocationLibrary"
